60d719bc0b4a232ed5f8d83b9da91590b55f4e46
Momofil31 <filippomomesso@gmail.com>
2021-03-22 22:17:23 +0100

    Punto 1

---
+++ index.php
@@ -0,0 +1,35 @@
+<html>
+
+<head>
+  <title>Esercizio 1 Tutorato</title>
+  <style>
+  .columns-div {
+    column-count: 3;
+    column-gap: 40px;
+    width: 70%;
+    margin: 0 auto;
+  }
+
+  table,
+  th,
+  td {
+    border: solid 1px black;
+  }
+  </style>
+</head>
+
+<body>
+  <h1>Esercizio 1 Tutorato</h1>
+  <div class="columns-div">
+    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto illum velit exercitationem non nobis est quia ipsa
+    quis architecto ipsum harum quas officia nemo reprehenderit expedita, in optio dolorem! Ad, velit labore eveniet ab
+    explicabo, molestias eum quasi repellendus incidunt aliquid sed aperiam error. Nobis voluptatum quae incidunt nemo
+    quas obcaecati quis dolore hic neque mollitia assumenda ea quia ut sunt id asperiores error enim consequuntur
+    dolores, nisi corporis debitis aliquid perspiciatis deleniti. Doloribus ipsa distinctio eius? Maxime totam facilis
+    maiores voluptatem ducimus architecto distinctio, fuga eos nesciunt minima voluptates magnam officiis facere?
+    Molestiae esse rerum sequi modi fugit nulla.
+  </div>
+  <br>
+</body>
+
+</html>
